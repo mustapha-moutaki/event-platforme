f5951221b017e166880ef309584b8e768e928a68
yassine <yassineaghla23@gmail.com>
2025-02-07 15:43:59 +0100

    config base de donnees

--- App/config/config.php
+++ App/config/config.php
@@ -3,7 +3,7 @@
 return [
     'db' => [
         'host' => 'localhost',
-        'port' => '3306',  
+        'port' => '3376',  
         'dbname' => 'utilisateur',
         'user' => 'root',
         'password' => ''
