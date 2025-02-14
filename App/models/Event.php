8a7103443afa17210d8a9a4b2512460493df9ac6
mustapha <mustaphastar067@gmail.com>
2025-02-14 23:49:55 +0100

    working on search box and filtering

--- App/models/Event.php
+++ App/models/Event.php
@@ -150,11 +150,4 @@ class Event extends BaseModel
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-
-    public function allevents(){
-        $query = "SELECT * FROM events";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt -> fetchAll(PDO::FETCH_ASSOC);
-        }
 }
--- App/view/action.php
+++ App/view/action.php
@@ -1,1 +1,31 @@
 <?php
+$conn = mysqli_connect("localhost", "root", "", "events_db");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['input'])) {
+    $search = $_POST['input'];
+    
+    $query = "SELECT * FROM events WHERE title LIKE '%$search%'";
+} else {
+    
+    $query = "SELECT * FROM events WHERE 0";
+}
+
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+    echo "Error: " . mysqli_error($conn);
+}
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div class='event'>";
+        echo "<h3>" . $row['title'] . "</h3>";
+        echo "<p>" . $row['description'] . "</p>";
+        echo "</div>";
+    }
+} else {
+}
+?>
